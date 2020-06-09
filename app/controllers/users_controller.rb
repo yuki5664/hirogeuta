0ba80532984b72046cde75ab05bf2c21b6241756
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-09 12:40:25 +0900

    Add user controllerのbeforeアクションの訂正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
-  before_action :corrent_user, only: [:show, :edit, :update]
+  before_action :corrent_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
