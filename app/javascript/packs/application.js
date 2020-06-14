ac7ca4f146728b73dccc1bfbd37f2ee0aba2b036
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-14 10:25:23 +0900

    herokuデプロイのため背景画像・フォント反映のための設定変更2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ require("jquery")
 require("bootstrap")
 
 require("custom/jquery.jscroll.min")
-require("stylesheets/custom.scss")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
