3f9d596838e0bf204930fe54da6b40992e76f242
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-15 23:39:25 +0900

    Add youtube_validationの修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,7 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :artist, presence: true, length: { maximum: 50 }
   validates :song, presence: true, length: { maximum: 50 }
-  validates :youtube_url, presence: true, length: { maximum: 50 }
+  VALID_YOUTUBE_URL = /\A[a-zA-Z0-9]{11}+\z/
+  validates :youtube_url, format: { with: VALID_YOUTUBE_URL }
   validates :content, presence: true, length: { maximum: 250 }
 end
