659305fd1b63277b171fed01e3b45c2a0039fc2f
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-20 23:24:56 +0900

    データベース反映

--- db/migrate/20200620140430_create_bookmarks.rb
+++ db/migrate/20200620140430_create_bookmarks.rb
@@ -1,11 +1,13 @@
 class CreateBookmarks < ActiveRecord::Migration[6.0]
-  def change
-  create_table :bookmarks do |t|
-    t.references :user, foreign_key: true, null: false
-    t.references :micropost, foreign_key: true, null: false
 
-    t.timestamps
-    t.index [:user_id, :micropost_id], unique: true # 追加
+  def change
+    create_table :bookmarks do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :micropost, foreign_key: true, null: false
+
+      t.timestamps
+      t.index [:user_id, :micropost_id], unique: true # 追加
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_063510) do
+ActiveRecord::Schema.define(version: 2020_06_20_140430) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_06_09_063510) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "micropost_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["micropost_id"], name: "index_bookmarks_on_micropost_id"
+    t.index ["user_id", "micropost_id"], name: "index_bookmarks_on_user_id_and_micropost_id", unique: true
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -72,6 +82,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_063510) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "microposts"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "likes", "microposts"
   add_foreign_key "likes", "users"
   add_foreign_key "microposts", "users"
