ecdd10e5b397699e03d177eb28c30452d2049afd
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-20 23:13:25 +0900

    とちゅう

---
+++ db/migrate/20200620140430_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[6.0]
+  def change
+  create_table :bookmarks do |t|
+    t.references :user, foreign_key: true, null: false
+    t.references :micropost, foreign_key: true, null: false
+
+    t.timestamps
+    t.index [:user_id, :micropost_id], unique: true # 追加
+  end
+
+end
