7291c3e742044a60415251f7d21793d946266e56
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-21 10:20:54 +0900

    Add bookmark機能のajex化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,19 @@
 class BookmarksController < ApplicationController
+  before_action :set_post, only: %i[create destroy]
+
   def create
     bookmark = current_user.bookmarks.build(micropost_id: params[:micropost_id])
     bookmark.save!
-    redirect_to root_path
   end
 
   def destroy
     current_user.bookmarks.find_by(micropost_id: params[:micropost_id]).destroy!
-    redirect_to root_path
+  end
+
+  private
+
+  def set_post
+    @post = Micropost.find(params[:micropost_id])
   end
 
 end
