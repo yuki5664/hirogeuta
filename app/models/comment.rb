799087bf3b6636282a1d327cf3d82f046575ffc5
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-06-28 18:01:44 +0900

    Add commentの表示順の変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   belongs_to :micropost
   belongs_to :user
   validates :body, presence: true, length: { maximum: 140}
